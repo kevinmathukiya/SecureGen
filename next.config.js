9aee9d09adce6102e2294e0dfdecd116e00e237a
kevinmathukiya <kevinmathukiya396@gmail.com>
2025-12-09 21:42:06 +0530

    Add redirects to enforce HTTPS and handle www subdomain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        permanent: true,
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://passwordgens.online/:path*',
+      },
+      {
+        source: '/:path*',
+        permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'www.passwordgens.online',
+          },
+        ],
+        destination: 'https://passwordgens.online/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
