042a4625a1a6b2ea5f94c0e91a831e4ef4ed87f6
kakadiyas682 <kakadiyas682@gmail.com>
2026-03-27 11:34:40 +0530

    Add Clarity tracking script to layout for enhanced user analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,6 +48,13 @@ export default function RootLayout({ children }) {
 
           gtag('config', '${siteConfig.analytics.google}');`}
         </Script>
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w26b71budh");`}
+        </Script>
 
         <Script
           id="organization-schema"
